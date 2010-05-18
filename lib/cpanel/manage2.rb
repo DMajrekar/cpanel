717a95466a939dbe280a46282bd5135ae20a8044
Dinesh Majrekar <d.majrekar@gmail.com>
2010-05-18 10:58:41 +0100

    Added Cpanel::Manage2

---
+++ lib/cpanel/manage2.rb
@@ -0,0 +1,94 @@
+require 'cgi'
+
+module Cpanel
+  class Manage2
+    MANAGE_2_URL = "https://manage2.cpanel.net"
+
+    attr_reader :username, :password
+
+    # The +username+ and +password+ for the manage2 account is required
+    #
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
+
+    # Get the license information from Manage2 using 'XMLlicenseInfo'. Returns a Cpanel::Reponse. Access to
+    # the license hash is through the +license+ method on the Cpanel:Reponse
+    #
+    #   manage2.licenses #=> Cpanel::Reponse
+    #
+    def licenses
+      Cpanel::Response::Yaml.new(get('/XMLlicenseInfo.cgi?output=yaml'))
+    end
+
+    # Add a license for the given +ip_address+ to the manage 2 account. The Terms and conditions are automatically accepted.
+    #
+    # +options+ is a hash that may contain:
+    #   * groupid   - defaults to the first group from Cpanel::Manage2#groups
+    #   * packageid - defaults to the first package from Cpanel::Manage2#packages
+    #   * force     - defaults to 0
+    #
+    def add_license(ip_address, options = {})
+      api_options = {
+        'ip'        => ip_address,
+        'legal'     => 1,
+        'groupid'   => options['groupid']   || options[:groupid]   || groups.keys.first,
+        'packageid' => options['packageid'] || options[:packageid] || packages.keys.first,
+        'force'     => options['force']     || options[:force]     || '0',
+        'output'    => 'yaml'
+      }
+
+      # manage2 api docs recomend a post but Net::HTTP::Post requires all values in post data to be a string.
+      # The manage2 api does not support legal being a string so we need to do a get request
+      get_url = '/XMLlicenseAdd.cgi?' + api_options.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
+      return Cpanel::Response::Yaml.new(get(get_url))
+    end
+
+    # Get the current groups from Manage2 using 'XMLgroupInfo'. Returns a Cpanel::Response. Access to the
+    # groups has is through the +groups+ method on the Cpanel::Response
+    #
+    #   manage2.groups #=> Cpanel::Response
+    #
+    def groups
+      Cpanel::Response::Yaml.new(get('/XMLgroupInfo.cgi?output=yaml'))
+    end
+
+    # Get the current packages from Manage2 using 'XMpackageInfo'. Returns a Cpanel::Response. Access to the
+    # packages has is through the +packages+ method on the Cpanel::Response
+    #
+    #   manage2.groups #=> Cpanel::Response
+    #
+    def packages
+      Cpanel::Response::Yaml.new(get('/XMLpackageInfo.cgi?output=yaml'))
+    end
+
+    private
+  
+    # Make a get request to manage 2 and return the response
+    #
+    def get(api_method) #:nodoc:
+      do_request(Net::HTTP::Get.new(api_method))
+    end
+
+    # Make a post request to manage 2 and return the response
+    #
+    def post(api_method, options) #:nodoc:
+      do_request(Net::HTTP::Post.new(api_method, options))
+    end
+
+    # Make a request to manage 2 using +req+ and return the response
+    #
+    def do_request(req) #:nodoc:
+#   raise req.to_hash.inspect
+      response = nil
+      url = URI.parse(MANAGE_2_URL)
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      req.basic_auth(username, password)
+      return http.request(req)
+    end
+  end
+end
---
+++ spec/cpanel/manage2_spec.rb
@@ -0,0 +1,192 @@
+require 'spec_helper'
+
+describe Cpanel::Manage2 do
+  describe ".new" do
+    it "should store the username" do
+      @client = Cpanel::Manage2.new('username', 'password')
+      @client.username.should == 'username'
+    end
+
+    it "should store the password" do
+      @client = Cpanel::Manage2.new('username', 'password')
+      @client.password.should == 'password'
+    end
+  end
+  
+  describe "#add_license" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+
+      @client.stubs(:groups).returns({ 'group_id' => {}})
+      @client.stubs(:packages).returns({ 'package_id' => {}})
+
+      @response = stub("Response Object")
+      Cpanel::Response::Yaml.stubs(:new).returns(@response)
+
+      @get_response = stub("Get Response")
+      @client.stubs(:get).returns(@get_response)
+    end
+    
+    it "should make a get request to '/XMLlicenseAdd.cgi'" do
+      @client.expects(:get).with(regexp_matches(/^\/XMLlicenseAdd.cgi.*$/))
+      @client.add_license('127.0.0.1')
+    end
+    
+    it "should set legal to 1" do
+      @client.expects(:get).with(regexp_matches(/legal=1/))
+      @client.add_license('127.0.0.1')      
+    end
+    
+    it "should set force to 0" do
+      @client.expects(:get).with(regexp_matches(/force=0/))
+      @client.add_license('127.0.0.1')      
+    end
+    
+    it "should set the ip address" do
+      @client.expects(:get).with(regexp_matches(/ip=127\.0\.0\.1/))
+      @client.add_license('127.0.0.1')      
+    end
+    
+    it "should set the group id" do
+      @client.expects(:get).with(regexp_matches(/groupid=group_id/))
+      @client.add_license('127.0.0.1')      
+    end
+    
+    it "should set the package id to that provided" do
+      @client.expects(:get).with(regexp_matches(/groupid=new_group_id/))
+      @client.add_license('127.0.0.1', {:groupid => 'new_group_id'})
+    end
+
+    it "should set the package id to that provided" do
+      @client.expects(:get).with(regexp_matches(/groupid=new_group_id/))
+      @client.add_license('127.0.0.1', {'groupid' => 'new_group_id'})
+    end
+    
+    it "should set the package id" do
+      @client.expects(:get).with(regexp_matches(/packageid=package_id/))
+      @client.add_license('127.0.0.1')      
+    end
+    
+    it "should set the package id to that provided" do
+      @client.expects(:get).with(regexp_matches(/packageid=new_package_id/))
+      @client.add_license('127.0.0.1', {:packageid => 'new_package_id'})
+    end
+
+    it "should set the package id to that provided" do
+      @client.expects(:get).with(regexp_matches(/packageid=new_package_id/))
+      @client.add_license('127.0.0.1', {'packageid' => 'new_package_id'})
+    end
+
+    it "should create a Yaml response object" do
+      Cpanel::Response::Yaml.expects(:new).with(@get_response).returns(@response)
+      @client.licenses
+    end
+
+    it "should return the reponse object" do
+      @client.licenses.should eql(@response)
+    end
+  end
+
+  describe "#licenses" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+
+      @response = stub("Response Object")
+      Cpanel::Response::Yaml.stubs(:new).returns(@response)
+
+      @get_response = stub("Get Response")
+      @client.stubs(:get).returns(@get_response)
+    end
+
+    it "should make a get request to '/XMLlicenseInfo.cgi?output=yaml'" do
+      @client.expects(:get).with('/XMLlicenseInfo.cgi?output=yaml').returns(@get_response)
+      @client.licenses
+    end
+
+    it "should create a Yaml response object" do
+      Cpanel::Response::Yaml.expects(:new).with(@get_response).returns(@response)
+      @client.licenses
+    end
+
+    it "should return the reponse object" do
+      @client.licenses.should eql(@response)
+    end
+  end
+
+  describe "#groups" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+
+      @response = stub("Response Object")
+      Cpanel::Response::Yaml.stubs(:new).returns(@response)
+
+      @get_response = stub("Get Response")
+      @client.stubs(:get).returns(@get_response)
+    end
+
+    it "should make a get request to '/XMLgroupInfo.cgi?output=yaml'" do
+      @client.expects(:get).with('/XMLgroupInfo.cgi?output=yaml').returns(@get_response)
+      @client.groups
+    end
+
+    it "should create a Yaml response object" do
+      Cpanel::Response::Yaml.expects(:new).with(@get_response).returns(@response)
+      @client.groups
+    end
+
+    it "should return the reponse object" do
+      @client.groups.should eql(@response)
+    end
+  end
+
+  describe "#packages" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+
+      @response = stub("Response Object")
+      Cpanel::Response::Yaml.stubs(:new).returns(@response)
+
+      @get_response = stub("Get Response")
+      @client.stubs(:get).returns(@get_response)
+    end
+
+    it "should make a get request to '/XMLpackageInfo.cgi?output=yaml'" do
+      @client.expects(:get).with('/XMLpackageInfo.cgi?output=yaml').returns(@get_response)
+      @client.packages
+    end
+
+    it "should create a Yaml response object" do
+      Cpanel::Response::Yaml.expects(:new).with(@get_response).returns(@response)
+      @client.packages
+    end
+
+    it "should return the reponse object" do
+      @client.packages.should eql(@response)
+    end
+  end
+
+  describe "#get (private method)" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+    end
+
+    it "should get to the given url with the given data" do
+      stub_request(:any, 'https://username:password@manage2.cpanel.net/test_api.cgi')
+      @client.send(:get, "/test_api.cgi")
+      WebMock.should have_requested(:get, "https://username:password@manage2.cpanel.net/test_api.cgi")
+    end
+  end
+
+  describe "#post (private method)" do
+    before(:each) do
+      @client = Cpanel::Manage2.new('username', 'password')
+    end
+
+    it "should post to the given url with the given data" do
+      stub_request(:any, 'https://username:password@manage2.cpanel.net/test_api.cgi')
+      @client.send(:post, "/test_api.cgi", {'key' => 'value'})
+      WebMock.should have_requested(:post, "https://username:password@manage2.cpanel.net/test_api.cgi").with( :headers => {'key' => 'value'} )
+    end
+  end
+
+end
